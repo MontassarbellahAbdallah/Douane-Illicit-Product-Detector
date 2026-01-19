698185dd44b70eab9e79c9d4e7df7d95d006bfc3
Montassar bellah ABDALLAH <montassar.bellah.abdallah@gmail.com>
2026-01-19 20:57:31 +0100

    update model

--- app/src/config.py
+++ app/src/config.py
@@ -27,7 +27,7 @@ output_dir = "./ai-agent-output"
 os.makedirs(output_dir, exist_ok=True)
 
 basic_llm = LLM(
-    model="gemini/gemini-2.5-flash-lite",
+    model="gemini/gemini-2.5-flash",
     temperature=0.7
 )
 
